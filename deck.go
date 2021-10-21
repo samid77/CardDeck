3d528a44d40e0de48ba241758a35998833f9d191
Samid77 <dimaseptyanto@windowslive.com>
2021-10-21 08:32:22 +0700

    Create a new function outside main

--- deck.go
+++ deck.go
@@ -4,6 +4,23 @@ import "fmt"
 
 type deck []string
 
+//Create new deck of cards and return list of cards/deck
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+//Log out all contents of a deck of cards
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
