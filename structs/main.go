419b30a3267c76af3fde665d99e0ab4caa7683a9
Samid77 <dimaseptyanto@windowslive.com>
2021-11-21 09:50:59 +0700

    Add pointers

---
+++ structs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Neutron",
+		contactInfo: contactInfo{
+			email:   "jim@mail.com",
+			zipCode: 299292,
+		},
+	}
+
+	jimPointer := &jim
+	jimPointer.updateName("Alex")
+	jim.print()
+
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
