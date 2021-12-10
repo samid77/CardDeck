0c754dbefe7bee83b57c98908ca9a14b66afd0c8
Samid77 <dimasseptyantop@gmail.com>
2021-12-10 09:40:47 +0700

    Iterating over Maps

--- map/main.go
+++ map/main.go
@@ -6,7 +6,18 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
 
-	fmt.Println(colors)
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+	// delete(colors, "white")
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, "is", hex)
+	}
 }
