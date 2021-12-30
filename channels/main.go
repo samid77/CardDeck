ff465aa3825855e72a5eb25bd75ae68b20e3ae55
Samid77 <dimasseptyantop@gmail.com>
2021-12-30 10:55:00 +0700

    More in channels and go routine

--- channels/main.go
+++ channels/main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, l := range links {
+		go checkLink(l, c)
+	}
+
+	// fmt.Println(<-c) // blocking operation
+	// fmt.Println(<-c) // blocking operation
+	// fmt.Println(<-c) // blocking operation
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	for l := range c {
+		// go checkLink(<-c, c)
+		// go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down ")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up !")
+	c <- link
+}
